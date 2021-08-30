2168db931e73e9353b28d37526573159c9965e79
Nolo Marsh <nolo.marsh@gmail.com>
2021-08-30 10:14:11 -0400

    fiddled with front end to try to work with back end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
+import React, { useState } from 'react'
 import './App.css'
 import Documenu from 'documenu'
+import axios from 'axios'
 
 const App = () => {
     Documenu.configure('d51fb5ef4342fbe99b76d644f8000896')
+    let [newUsername, setNewUsername] = useState('')
+    let [newPassword, setNewPassword] = useState('')
 
     const getRestaurantsByZip = zipCode => {
         Documenu.Restaurants.getByZipCode(zipCode, {fullmenu: true})
@@ -12,11 +15,33 @@ const App = () => {
         })
     }
 
-    const getMenuItemsByGeo = (lat, lon, distance) => {
-        Documenu.MenuItems.searchGeo(lat, lon, distance)
-        .then((response) => {
+    // const getMenuItemsByGeo = (lat, lon, distance) => {
+    //     Documenu.MenuItems.searchGeo(lat, lon, distance)
+    //     .then((response) => {
+    //         console.log(response.data)
+    //     })
+    // }
+
+    const testGet = () => {
+        axios.get('http://localhost:8080/users').then((response) => {
             console.log(response.data)
-        })
+            }
+        )
+    }
+
+    const testPost = () => {
+        axios.post('http://localhost:8080/users', {username: newUsername, password: newPassword})
+            .then((response) => {
+                console.log(response.data)
+            })
+    }
+
+    const handleChangeNewUsername = e => {
+        setNewUsername(e.target.value)
+    }
+
+    const handleChangeNewPassword = e => {
+        setNewPassword(e.target.value)
     }
 
 
@@ -24,7 +49,13 @@ const App = () => {
         <>
         <h1>Hello World</h1>
         <button onClick={()=>getRestaurantsByZip('49002')}>test 1</button>
-        <button onClick={()=>getMenuItemsByGeo(42.238443,-85.589471,1)}>test2</button>
+        <button onClick={testGet}>test2</button>
+        <form onSubmit={testPost}>
+            <input type="text" placeholder="username" onChange={handleChangeNewUsername}/>
+            <input type="password" placeholder="password" onChange={handleChangeNewPassword}/>
+            <input type="submit" value="submit"/>
+        </form>
+
         </>
     )
 }
